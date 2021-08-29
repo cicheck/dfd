e7d8369d0b3fb6c63b5ec00efb989c6c6fb866d8
Igor Cichecki <cicheckiigor@gmail.com>
2021-08-29 22:15:41 +0200

    docs: make generator docstring more precise

--- src/dfd/datasets/generator.py
+++ src/dfd/datasets/generator.py
@@ -1,7 +1,5 @@
 """Generate new dataset after performing set non malicious frames modifications.
 
-Generator input should by generated via datasets package.
-
 Assumption:
 Input directory contains two sub directories:
     * reals: contains frames from real videos
