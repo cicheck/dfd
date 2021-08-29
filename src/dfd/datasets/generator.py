e9abf5d3aba7028f865bdaebecbee8d292f2b3e0
Igor Cichecki <cicheckiigor@gmail.com>
2021-08-29 22:13:13 +0200

    feat: initialize dataset generator

---
+++ src/dfd/datasets/generator.py
@@ -0,0 +1,14 @@
+"""Generate new dataset after performing set non malicious frames modifications.
+
+Generator input should by generated via datasets package.
+
+Assumption:
+Input directory contains two sub directories:
+    * reals: contains frames from real videos
+    * fakes: contains frames fom fake videos
+
+"""
+
+
+class DatasetGenerator:
+    pass
