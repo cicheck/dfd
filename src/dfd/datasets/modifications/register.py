3bd79e02ed16a2c2656b2132d8be057232688b70
Igor Cichecki <cicheckiigor@gmail.com>
2021-09-01 22:01:54 +0200

    refactor: make mapping in ModificationRegister private

--- src/dfd/datasets/modifications/register.py
+++ src/dfd/datasets/modifications/register.py
@@ -1,6 +1,7 @@
 """Modifications register."""
 from typing import Dict, Type
 
+from ...exceptions import DfdError
 from .definitions import (
     CLAHEModification,
     GammaCorrectionModification,
@@ -15,11 +16,15 @@ class ModificationRegister:
     """Define available modifications."""
 
     def __init__(self, name_to_modification_map: NameToModificationTypeMap) -> None:
-        self.name_to_modification_type_map = name_to_modification_map
+        self._name_to_modification_type_map = name_to_modification_map
 
     @classmethod
     def default(cls) -> "ModificationRegister":
-        """Create ModificationRegister with default modifications."""
+        """Create ModificationRegister with default modifications.
+
+        Returns:
+            ModificationRegister with default modifications registered.
+        """
 
         return cls(
             {
@@ -28,3 +33,15 @@ class ModificationRegister:
                 HistogramEqualizationModification.name(): HistogramEqualizationModification,
             },
         )
+
+    def get_modification(self, modification_name: str) -> Type[ModificationInterface]:
+        """Get registered modification via name.
+
+        Returns:
+            Type of registered modification.
+
+        """
+        try:
+            return self._name_to_modification_type_map[modification_name]
+        except KeyError:
+            raise DfdError(f"Modification {modification_name} is not registered.")
