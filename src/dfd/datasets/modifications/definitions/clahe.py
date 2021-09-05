0fd43e051d6b11148702d2d987d2fd7d35fca39e
Igor Cichecki <cicheckiigor@gmail.com>
2021-09-05 13:37:46 +0200

    feat: allow to parametrize CLAHE modification

--- src/dfd/datasets/modifications/definitions/clahe.py
+++ src/dfd/datasets/modifications/definitions/clahe.py
@@ -1,5 +1,4 @@
 """Modification CLAHE (Contrast Limited Adaptive Histogram Equalization)."""
-from typing import Tuple
 
 import cv2 as cv
 import numpy as np
@@ -10,16 +9,17 @@ from dfd.datasets.modifications.interfaces import ModificationInterface
 class CLAHEModification(ModificationInterface):
     """Modification CLAHE (Contrast Limited Adaptive Histogram Equalization)"""
 
-    def __init__(self, clip_limit: float, tile_grid_size: Tuple[int, int]) -> None:
+    def __init__(self, clip_limit: float, grid_width: int, grid_height: int) -> None:
         """Initialize AdaptiveHistogramEqualizationModification.
 
         Args:
             clip_limit: limit used to define opencv CLAHE object, as in cv.createCLAHE
-            tile_grid_size: tile grid size used to define opencv CLAHE object, as in cv.createCLAHE
+            grid_width: tile grid width used to define opencv CLAHE object, as in cv.createCLAHE
+            grid_height: tile grid height used to define opencv CLAHE object, as in cv.createCLAHE
         """
 
         self._clip_limit = clip_limit
-        self._title_grid_size = tile_grid_size
+        self._title_grid_size = (grid_width, grid_height)
 
     @classmethod
     def name(cls) -> str:
@@ -47,7 +47,7 @@ class CLAHEModification(ModificationInterface):
         # Convert from BGR color space to YCrCb
         ycrcb_image = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
         # Prepare CLAHE
-        clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        clahe = cv.createCLAHE(clipLimit=self._clip_limit, tileGridSize=self._title_grid_size)
         # Equalize y channel
         ycrcb_image[:, :, 0] = clahe.apply(ycrcb_image[:, :, 0])
         # Convert back to BGR
