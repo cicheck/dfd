4ff424695b06f2eace6e9cd62cfa22ab63e1facb
Igor Cichecki <cicheckiigor@gmail.com>
2021-10-05 20:17:14 +0200

    refactor: remove obsolete method from CelebDFExtractor

--- src/dfd/datasets/celeb_df.py
+++ src/dfd/datasets/celeb_df.py
@@ -2,15 +2,10 @@
 from pathlib import Path
 from typing import Optional
 
-import cv2
-import numpy as np
-from tqdm import tqdm
-
-from src.dfd.datasets.converters import convert_video_to_frames
-
 from .frame_extractor import FrameExtractor
 
 
+# TODO: obsolete faced, should be removed
 class CelebDFExtractor:
     """Extract frames from raw celeb-DF.
 
@@ -95,37 +90,3 @@ class CelebDFExtractor:
             lower_bound=lower_bound,
             upper_bound=upper_bound,
         )
-
-    def _extract_batch(
-        self,
-        input_path: Path,
-        output_path: Path,
-        lower_bound: Optional[int],
-        upper_bound: Optional[int],
-    ) -> None:
-        """Extract frames from batch of videos.
-
-        Split videos into frames and save them into output directory.
-        If boundaries are not specified frames_extractor all videos from input directory.
-        Frames are saved in files named by number in which they were produced.
-        Starting number is determined by number of files already existing in directory.
-
-        Args:
-            input_path: path to directory containing videos.
-            output_path: path to directory where frames from videos should be saved.
-            lower_bound: lower batch boundary.
-            upper_bound: upper batch boundary.
-
-        """
-        all_input_videos = sorted(input_path.iterdir())
-        processed_input_videos = all_input_videos[lower_bound:upper_bound]
-        for video in tqdm(processed_input_videos):
-            video_frames = convert_video_to_frames(filepath=str(video))
-            video_prefix = video.name.split(".")[0]
-            for frame_index, frame in enumerate(video_frames):
-                frame_path = output_path.joinpath("{0}_{1}.png".format(video_prefix, frame_index))
-                self._save_video_frame(frame, str(frame_path))
-
-    @staticmethod
-    def _save_video_frame(frame: np.ndarray, filepath: str) -> None:
-        cv2.imwrite(filepath, frame)
