eb4e56ca2615bf6d4b3f2bad852663715fe6b833
Igor Cichecki <cicheckiigor@gmail.com>
2021-10-08 19:55:48 +0200

    fix: proper __str__ dunder method in modifications

--- src/dfd/datasets/modifications/definitions/gaussian_blur.py
+++ src/dfd/datasets/modifications/definitions/gaussian_blur.py
@@ -1,5 +1,4 @@
 """Modification Gaussian blur (AKA Gaussian smoothing)."""
-from typing import Optional, Tuple
 
 import cv2 as cv
 import numpy as np
@@ -12,22 +11,27 @@ class GaussianBlurModification(ModificationInterface):
 
     def __init__(
         self,
-        kernel_size: Optional[Tuple[int, int]] = (5, 5),
+        kernel_width: int = 0,
+        kernel_height: int = 0,
         sigma_x: int = 0,
         sigma_y: int = 0,
     ) -> None:
         """Initialize GaussianBlurModification.
 
         Args:
-            kernel_size: Gaussian kernel size used by OpenCV, it needs to be a pair of odd ints,
-                or zeros if sigma values should be used to compute kernel automatically.
+            kernel_width: Width of kernel used by OpenCV, should be zero or odd int.
+            kernel_height: Height of kernel used by OpenCV, should be zero or odd int.
             sigma_x: Gaussian kernel standard deviation in X direction used by OpenCV.
             sigma_y: Gaussian kernel standard deviation in Y direction used by OpenCV.
 
         """
-        self._kernel_size = kernel_size
+        self._kernel_size = (kernel_width, kernel_height)
         self._sigma_x = sigma_x
         self._sigma_y = sigma_y
+
+    def __str__(self) -> str:
+        width, height = self._kernel_size
+        return f"gaussian_blur{width}_{height}_{self._sigma_x}_{self._sigma_y}"
 
     def perform(self, image: np.ndarray) -> np.ndarray:
         """Perform Gaussian blur on provided image.
