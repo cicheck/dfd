070e87fa96518e57761d69c320d3d351e49da6ad
Igor Cichecki <cicheckiigor@gmail.com>
2021-10-14 20:23:11 +0200

    refactor: use extracted function in CLI command split

--- src/dfd/cli/split.py
+++ src/dfd/cli/split.py
@@ -3,9 +3,8 @@ import pathlib
 from typing import Optional
 
 import click
-import random
 
-from tqdm import tqdm
+from dfd.datasets import split as _split
 
 
 @click.command(name="split")
@@ -53,47 +52,13 @@ def split(
         )
         return
     if train + validation + test != 1:
-        click.echo(
-            "Ratios [train, validation, test] must adds to one."
-        )
+        click.echo("Ratios [train, validation, test] must adds to one.")
         return
     click.echo(f"Splitting with ratios [train: {train}, validation: {validation}, test: {test}].")
-
-    class_directories = [path for path in input_path.iterdir() if path.is_dir()]
-    # TODO: loop block to private function
-    for class_dir in class_directories:
-        all_files = [path for path in class_dir.rglob("*") if path.is_file()]
-        no_files = len(all_files)
-        # Shuffle files
-        random.shuffle(all_files)
-        # Split
-        validation_lower_bound = int(train * no_files)
-        test_lower_bound = int((train + validation) * no_files)
-        # Move files
-        train_files = all_files[:validation_lower_bound]
-        validation_files = all_files[validation_lower_bound:test_lower_bound]
-        test_files = all_files[test_lower_bound:]
-        click.echo(
-            "From class {0} using {1} for train, {2} for validation and {3} for test.".format(
-                class_dir.name,
-                len(train_files),
-                len(validation_files),
-                len(test_files),
-            )
-        )
-        # TODO: function instead of 3 loops
-        for file in tqdm(train_files, desc="train"):
-            path_relative_to_input_dir = file.relative_to(input_path)
-            moved_file_path = output_path / "train" / path_relative_to_input_dir
-            moved_file_path.parent.mkdir(exist_ok=True, parents=True)
-            file.replace(moved_file_path)
-        for file in tqdm(validation_files, desc="validation"):
-            path_relative_to_input_dir = file.relative_to(input_path)
-            moved_file_path = output_path / "validation" / path_relative_to_input_dir
-            moved_file_path.parent.mkdir(exist_ok=True, parents=True)
-            file.replace(moved_file_path)
-        for file in tqdm(test_files, desc="test"):
-            path_relative_to_input_dir = file.relative_to(input_path)
-            moved_file_path = output_path / "test" / path_relative_to_input_dir
-            moved_file_path.parent.mkdir(exist_ok=True, parents=True)
-            file.replace(moved_file_path)
+    _split(
+        input_path=input_path,
+        output_path=output_path,
+        train_ds_share=train,
+        validation_ds_share=validation,
+        test_ds_share=test,
+    )
