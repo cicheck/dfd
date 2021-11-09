64bc27997183899842c650c1a71884732c8a3c31
Igor Cichecki <cicheckiigor@gmail.com>
2021-11-09 21:18:01 +0200

    test: add tests for models registry

---
+++ tests/ut/models/test_registry.py
@@ -0,0 +1,24 @@
+from types import MappingProxyType
+
+import pytest
+
+from dfd.exceptions import DfdError
+from dfd.models import ModelRegistry
+from tests.stubs.model import ModelStub
+
+REGISTERED_MODELS = MappingProxyType({"stub": ModelStub})
+
+
+def test_get_registered_model():
+    # Given
+    registry = ModelRegistry(REGISTERED_MODELS)
+    # When
+    actual_model_class = registry.get_model_class("stub")
+    # Then
+    assert actual_model_class is ModelStub
+
+
+def test_not_registered_model():
+    registry = ModelRegistry(REGISTERED_MODELS)
+    with pytest.raises(DfdError):
+        registry.get_model_class("non_existing_class")
