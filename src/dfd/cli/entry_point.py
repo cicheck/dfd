0f670bc57bc617d7c61d1b59ef95c60dc4deab17
Igor Cichecki <cicheckiigor@gmail.com>
2021-11-14 22:22:45 +0200

    refactor: remove obsolete split CLI method

--- src/dfd/cli/entry_point.py
+++ src/dfd/cli/entry_point.py
@@ -2,12 +2,8 @@
 
 import click
 
-from .face_extractor import extract_faces
-from .frames_extractor import extract_frames
-from .modification_generator import modify_frames
 from .predict import predict
 from .preprocess import preprocess
-from .split import split
 from .test import test
 from .train import train
 
@@ -18,10 +14,6 @@ def entry_point():
 
 
 entry_point.add_command(preprocess)
-entry_point.add_command(extract_frames)
-entry_point.add_command(extract_faces)
-entry_point.add_command(modify_frames)
 entry_point.add_command(predict)
-entry_point.add_command(split)
 entry_point.add_command(train)
 entry_point.add_command(test)
